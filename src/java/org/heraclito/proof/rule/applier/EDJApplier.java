df4f0412f8ed36c3fd2b76dcfb9b5058f71bae2c
rafaelkperes <rafaelkperes@gmail.com>
2015-08-21 14:17:35 -0300

    todo: 	- EDJTest 	- rules on proof 	- rule start, add param, apply 	- iterators for print on proof and lines + Applier + DNApplier + Several Rule modifications

--- src/java/org/heraclito/proof/rule/applier/EDJApplier.java
+++ src/java/org/heraclito/proof/rule/applier/EDJApplier.java
@@ -30,55 +30,58 @@ public class EDJApplier extends Applier {
 
         Expression disjunctionExpression, firstImplication, secondImplication;
 
-        if (Operator.DISJUNCTION.equals(firstInnerExpression.getMainOperator())) {            
+        if (Operator.DISJUNCTION.equals(firstInnerExpression.getMainOperator())) {
             if (!Operator.IMPLICATION.equals(secondInnerExpression.getMainOperator())
-                    || !Operator.IMPLICATION.equals(thirdInnerExpression.getMainOperator()))  {
+                    || !Operator.IMPLICATION.equals(thirdInnerExpression.getMainOperator())) {
                 throw new ProofException("exception_invalid_main_operator");
             }
-            
+
             disjunctionExpression = firstInnerExpression;
             firstImplication = secondInnerExpression;
             secondImplication = thirdInnerExpression;
         } else if (Operator.DISJUNCTION.equals(secondInnerExpression.getMainOperator())) {
             if (!Operator.IMPLICATION.equals(firstInnerExpression.getMainOperator())
-                    || !Operator.IMPLICATION.equals(thirdInnerExpression.getMainOperator()))  {
+                    || !Operator.IMPLICATION.equals(thirdInnerExpression.getMainOperator())) {
                 throw new ProofException("exception_invalid_main_operator");
             }
-            
+
             disjunctionExpression = secondInnerExpression;
             firstImplication = firstInnerExpression;
             secondImplication = thirdInnerExpression;
         } else if (Operator.DISJUNCTION.equals(thirdInnerExpression.getMainOperator())) {
             if (!Operator.IMPLICATION.equals(firstInnerExpression.getMainOperator())
-                    || !Operator.IMPLICATION.equals(secondInnerExpression.getMainOperator()))  {
+                    || !Operator.IMPLICATION.equals(secondInnerExpression.getMainOperator())) {
                 throw new ProofException("exception_invalid_main_operator");
             }
-            
+
             disjunctionExpression = thirdInnerExpression;
             firstImplication = firstInnerExpression;
             secondImplication = secondInnerExpression;
         } else {
             throw new ProofException("exception_invalid_main_operator");
         }
-                       
-        if(!firstImplication.getRightExpression()
+
+        if (!firstImplication.getRightExpression()
                 .equals(secondImplication.getRightExpression())) {
             throw new ProofException("exception_invalid_expression");
-        }        
-        
-        if(disjunctionExpression.getLeftExpression()
-                .equals(firstImplication.getLeftExpression())) {
-            if(disjunctionExpression.getRightExpression()
-                    .equals(secondImplication.getRightExpression())) {
-                
-            } else {
-                throw new ProofException("exception_invalid_expression");
-            }
-            
-            
         }
         
-        return new Expression("");
+        Expression leftDisjunction = disjunctionExpression.getLeftExpression();
+        Expression rightDisjunction = disjunctionExpression.getRightExpression();
+        Expression firstImpLeft = firstImplication.getLeftExpression();
+        Expression secondImpLeft = secondImplication.getRightExpression();
+        
+        if(!leftDisjunction.equals(firstImpLeft)) {
+            if(!rightDisjunction.equals(firstImpLeft)) {
+                throw new ProofException("exception_invalid_expression");
+            } else if(!leftDisjunction.equals(firstImpLeft)) {
+                throw new ProofException("exception_invalid_expression");
+            }
+        } else if(!rightDisjunction.equals(secondImpLeft)) {
+            throw new ProofException("exception_invalid_expression");
+        }
+
+        return firstImplication.getRightExpression();
     }
 
 }
